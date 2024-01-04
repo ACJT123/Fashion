5643835adcbf67f3cbcbe66fe54543811c39b8be
alex <alexcheng@vvinners.com>
2024-01-04 10:31:06 +0800

    top nav completed

--- resources/js/Components/TopNav.jsx
+++ resources/js/Components/TopNav.jsx
@@ -24,10 +24,26 @@ const links = [
 ];
 
 const TopNav = () => {
-    return <div className="m-0 fixed top-0 "><div className="logo-container">
-            <img src={Logo} alt="logo" className="w-30 h-30"/>
-            <h1>FASHION</h1>
-        </div></div>;
+    return (
+        <div className="m-0 fixed top-0 flex items-center font-bold justify-between w-full py-8 px-14">
+            <div className="logo-container flex gap-3 items-center">
+                <img src={Logo} alt="logo" className="w-7 h-7" />
+                <h1 className="font-logoText text-4xl">FASHION</h1>
+            </div>
+            <div className="nav-links flex gap-20">
+                {links.map((link) => (
+                    <div key={link.name}>
+                        <a
+                            href={link.url}
+                            className={`text-xl font-medium uppercase ${link.name === "signup" ? "text-white bg-black py-3 px-5 rounded-lg" : ""}`}
+                        >
+                            {link.name}
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default TopNav;
